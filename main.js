f7ab2748b1da86af30f431e3d8a03407dc3b57fd
“Boris <bmedved78@mail.ru>
2023-02-07 21:53:00 +0300

    initial

---
+++ main.js
@@ -0,0 +1,24 @@
+"use srtict";
+{
+  const goods = "Кроссовки";
+  const amountGoods = 7;
+  const categoryGoods = "Спорт";
+  const priceGoods = 4575;
+
+  const resault = amountGoods * priceGoods;
+
+  console.log(goods);
+  console.log(resault + " руб.");
+}
+
+{
+  const goods = "Спортивный костюм";
+  const amountGoods = 7;
+  const categoryGoods = "Спорт";
+  const priceGoods = 4575;
+
+  const resault = amountGoods * priceGoods;
+
+  console.log(goods);
+  console.log(resault + " руб.");
+}
