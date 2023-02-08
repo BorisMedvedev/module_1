c9a9158befa660791b685502f726852fb506b286
“Boris <bmedved78@mail.ru>
2023-02-08 19:17:26 +0300

    tasksHard_2

--- tasksHard_2.js
+++ tasksHard_2.js
@@ -1,12 +1,17 @@
+"use strict";
 {
   const finance = +prompt("Введите сумму вашего дохода");
   if (finance <= 15000) {
-    console.log(`Сумма налога: ${finance * 0.13}`);
+    console.log(`ваша сумма наолога ${finance * 0.13}`);
   }
   if (finance > 15000 && finance <= 50000) {
-    console.log(`Сумма налога: ${(finance - 15000) * 0.2}`);
+    console.log(
+      `ваша сумма наолога ${(finance - 15000) * 0.2 + finance * 0.13}`
+    );
   }
   if (finance > 50000) {
-    console.log(`Сумма налога: ${(finance - 50000) * 0.3}`);
+    console.log(
+      `ваша сумма наолога ${(finance - 15000) * 0.2 + finance * 0.13}`
+    );
   }
 }
