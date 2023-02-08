efc11edad92782a7d23e2c03c3d72d8dd85f1cd0
“Boris <bmedved78@mail.ru>
2023-02-08 20:50:45 +0300

    coment_del

--- tasks.js
+++ tasks.js
@@ -1,30 +1,30 @@
 "use strict";
-// {
-//   function getRandomInt(min, max) {
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     return Math.floor(Math.random() * (max - min)) + min;
-//   }
+{
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-//   let rain =
-//     getRandomInt(0, 2) === 1
-//       ? console.log("Пошёл дождь. Возьмите зонт!")
-//       : console.log("Дождя нет!");
-// }
+  let rain =
+    getRandomInt(0, 2) === 1
+      ? console.log("Пошёл дождь. Возьмите зонт!")
+      : console.log("Дождя нет!");
+}
 
-// {
-//   const mathematics = +prompt("Введите кол-во баллов по математике:");
-//   const russianlanguage = +prompt("Введите кол-во баллов по русскому языку:");
-//   const informatics = +prompt("Введите кол-во баллов по информатике:");
+{
+  const mathematics = +prompt("Введите кол-во баллов по математике:");
+  const russianlanguage = +prompt("Введите кол-во баллов по русскому языку:");
+  const informatics = +prompt("Введите кол-во баллов по информатике:");
 
-//   const resault = mathematics + russianlanguage + informatics;
+  const resault = mathematics + russianlanguage + informatics;
 
-//   if (resault >= 265) {
-//     console.log(`Поздравляю, вы поступили на бюджет! у вас ${resault} баллов`);
-//   } else {
-//     console.log(`Недостаточно баллов у вас ${resault} баллов`);
-//   }
-// }
+  if (resault >= 265) {
+    console.log(`Поздравляю, вы поступили на бюджет! у вас ${resault} баллов`);
+  } else {
+    console.log(`Недостаточно баллов у вас ${resault} баллов`);
+  }
+}
 
 {
   const many = +prompt("Введите сумму которую хотите снять");
