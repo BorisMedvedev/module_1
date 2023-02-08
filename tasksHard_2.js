64003037617e743907d8b3914cb44354d2c9a1fd
John Doe <johndoe@example.com>
2023-02-08 16:41:24 +0300

    add

--- tasksHard_2.js
+++ tasksHard_2.js
@@ -1,13 +1,12 @@
-"use strict";
 {
   const finance = +prompt("Введите сумму вашего дохода");
   if (finance <= 15000) {
     console.log(`Сумма налога: ${finance * 0.13}`);
   }
   if (finance > 15000 && finance <= 50000) {
-    console.log(`Сумма налога: ${finance * 0.2}`);
+    console.log(`Сумма налога: ${(finance - 15000) * 0.2}`);
   }
   if (finance > 50000) {
-    console.log(`Сумма налога: ${finance * 0.3}`);
+    console.log(`Сумма налога: ${(finance - 50000) * 0.3}`);
   }
 }
