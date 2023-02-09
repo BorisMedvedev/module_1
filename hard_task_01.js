f8cd542803672cd83f87047c69ca3114a6e492d3
“Boris <bmedved78@mail.ru>
2023-02-09 19:03:05 +0300

    fix

--- hard_task_01.js
+++
@@ -1,9 +0,0 @@
-const gcd = (a, b) => {
-  if (!b) {
-    return a;
-  }
-
-  return gcd(b, a % b);
-};
-
-console.log(gcd(10, 5));
--- tasks03.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-const reverseStr = (str) => str.split("").reverse().join("");
-
-console.log(reverseStr("Привет мир"));
