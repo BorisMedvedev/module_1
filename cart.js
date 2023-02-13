523efc1d3f9a3fc88e8a0dd6b9a3528ecd3812d8
John Doe <johndoe@example.com>
2023-02-13 16:02:52 +0300

    fix

--- cart.js
+++ cart.js
@@ -1,15 +1,16 @@
 "use strict";
-const allStudents = ["Петров", "Иванов", "Васильев", "Степанова", "Яковлева"];
-const failedStudents = ["Петров", "Иванов", "Васильев"];
 
-const generateValid = (allE, notPassed) => {
-  const successfullyPassed = [];
-  for (let indexAll in allE) {
-    if (!notPassed.includes(allE[indexAll])) {
-      successfullyPassed.push(allE[indexAll]);
-    }
-  }
-  return successfullyPassed;
+const cart = {
+  items: [],
+  totalPrice: 0,
+  count: 0,
+
+  getTotalPrice: () => cart.totalPrice,
+  calculateItemPrice: (cart) => {},
+  add: (nameGoods, priceGoods, quantityGoods = 1) => {},
 };
 
-console.log(generateValid(allStudents, failedStudents));
+console.log(cart.getTotalPrice());
+
+// clear: (cart) => {},
+// print: () => {},
