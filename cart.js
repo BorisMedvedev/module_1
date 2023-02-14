9d89d7c8d7f31c383ff4a0afadd1bd562243f061
John Doe <johndoe@example.com>
2023-02-14 10:46:06 +0300

    fix

--- cart.js
+++ cart.js
@@ -1,14 +1,32 @@
-"use strict";
+'use strict';
 
 const cart = {
   items: [],
   totalPrice: 0,
   count: 0,
-  getTotalPrice: () => {},
-  calculateItemPrice: (cart) => {},
-  add: (nameGoods, priceGoods, quantityGoods = 1) => {},
-  clear: (cart) => {},
-  print: () => {},
+  getTotalPrice() {
+    return this.totalPrice;
+  },
+  calculateItemPrice() {
+    return this.items.reduce((acc, item) => {
+      return acc + item.priceGoods * item.quantityGoods;
+    }, 0);
+  },
+  add(nameGoods, priceGoods, quantityGoods = 1) {
+    this.items.push({ nameGoods, priceGoods, quantityGoods });
+    this.increaseCount(quantityGoods);
+  },
+  clear() {
+    this.items = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  },
+  print() {
+    let printOut = JSON.stringify(this.items);
+    console.log(printOut);
+    return printOut;
+  },
+  increaseCount() {
+    return (this.count += quantityGoods);
+  },
 };
-
-console.log(cart.getTotalPrice());
