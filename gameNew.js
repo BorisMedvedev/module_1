1f854a8b667c692204b865a7da2b7892d37c629c
John Doe <johndoe@example.com>
2023-02-16 09:57:10 +0300

    fix

---
+++ gameNew.js
@@ -0,0 +1,13 @@
+'use strict';
+let input = '';
+const random = Math.floor(Math.random() * 100);
+
+while ((input = prompt('Введите число от 0 до 100:'))) {
+  if (isNaN(input)) alert('Введите число!');
+  else if (input > random) alert('Меньше!');
+  else if (input < random) alert('Больше!');
+  else if (input === random) {
+    alert('Правильно!');
+    break;
+  }
+}
