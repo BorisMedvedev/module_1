ff53dcf93c1d14cc4e4b1ed62d5f2829dceb6576
John Doe <johndoe@example.com>
2023-02-16 11:15:23 +0300

    fix_newBot

--- gameNew.js
+++ gameNew.js
@@ -1,19 +1,22 @@
 'use strict';
-// let input = '';
-// const random = Math.floor(Math.random() * 100);
 
-// while ((input = prompt('Введите число от 0 до 100:'))) {
-//   if (isNaN(input)) alert('Введите число!');
-//   else if (input > random) alert('Меньше!');
-//   else if (input < random) alert('Больше!');
-//   else if (input === random) {
-//     alert('Правильно!');
-//     break;
-//   }
-// }
 const random = Math.floor(Math.random() * 100);
-console.log(random);
-let input = '';
-const newBot = (num) => {};
+const newBot = (num) => {
+  const input = +prompt('Введите число от 0 до 100:');
+  if (isNaN(input)) alert('Введите число!');
+  if (input) {
+    if (input > num) {
+      alert('меньше');
+      newBot(num);
+    } else if (input < num) {
+      alert('Больше');
+      newBot(num);
+    } else {
+      alert('Правильно!');
+      return;
+    }
+  }
+  return;
+};
 
 newBot(random);
