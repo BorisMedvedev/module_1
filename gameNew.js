5867b604471221df6e6ed9a343565c9c3cb2740d
John Doe <johndoe@example.com>
2023-02-20 09:17:11 +0300

    fix

--- gameNew.js
+++ gameNew.js
@@ -3,8 +3,10 @@
 const random = Math.floor(Math.random() * 100);
 const newBot = (num) => {
   const input = prompt('Введите число от 0 до 100:');
+
   if (isNaN(input)) alert('Введите число!');
-  if (input > 0 && input !== '') {
+  if (input <= 0) alert('введите число больше 0');
+  if (input && input !== '') {
     if (input > num) {
       alert('меньше');
       newBot(num);
